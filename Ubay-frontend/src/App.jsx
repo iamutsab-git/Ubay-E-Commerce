b9761c8d1a4f7104542b43ce67f052c42417eaa9
iamutsab-git <utsabthapa8@gmail.com>
2025-08-06 21:36:24 +0545

    fixing bugs

--- Ubay-frontend/src/App.jsx
+++ Ubay-frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
 import About from './pages/About'
 import Collection from './pages/Collection'
 import SignUp from './pages/SignUp'
